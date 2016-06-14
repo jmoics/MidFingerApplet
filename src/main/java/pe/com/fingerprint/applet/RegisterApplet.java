9a96648db2bf72126bfc0276f6edf30752a7808e
jcueva <jmoics@gmail.com>
2016-06-14 16:05:50 -0500

    - change message of error when not find a scanner.

--- src/main/java/pe/com/fingerprint/applet/RegisterApplet.java
+++ src/main/java/pe/com/fingerprint/applet/RegisterApplet.java
@@ -170,7 +170,7 @@ public class RegisterApplet
                                 System.out.println("UFM_Create fail!! code :" + nRes);
                                 ScannerUtil.showErrorString(libScanner, nRes);
                                 ScannerUtil.MsgBox(
-                                                "Ocurrió un error al capturar su huella, por favor inténtelo nuevamente");
+                                                "Ocurrió un error inicializar librerías, por favor contacte al Administrador");
                             }
 
                         } else {
@@ -184,7 +184,7 @@ public class RegisterApplet
                 } else {
                     System.out.println("Init() fail!!");
                     ScannerUtil.showErrorString(libScanner, nRes);
-                    ScannerUtil.MsgBox("Ocurrió un error al capturar su huella, por favor inténtelo nuevamente");
+                    ScannerUtil.MsgBox("Ocurrió un error al cargar las librerías, por favor contacte al Administrador");
                 }
             } catch (final Exception ex) {
                 // setStatusMsg("loadLlibrary : UFScanner,UFMatcher fail!!");
